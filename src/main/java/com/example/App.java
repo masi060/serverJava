a6999bfe76f4ddcb41ae6231149ced338fe839c4
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 09:54:34 +0200

    Server creato

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,11 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            System.out.println("Server Acceso");
+            ServerSocket server = new ServerSocket(3000);
+            
+            Socket s = server.accept();
+
+            System.out.println("un client si e' connesso");
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            
+            System.out.println(" Creati input ed output");
+
+            String str = in.readLine();
+            System.out.println("stringa ricevuta");
+            str = str.toUpperCase();
+
+            out.writeBytes(str + "\n");
+            System.out.println("stringa inviata");
+            server.close();
+        
+        }   
+
+        catch(Exception e){
+            
+        }
     }
 }
